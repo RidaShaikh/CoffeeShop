29fd152662c3421a038cd5b271b5abd39de8946c
RidaShaikh304 <85176059+RidaShaikh@users.noreply.github.com>
2022-01-01 20:02:04 +0500

    Add files via upload

---
+++ src/coffeshop/Person.java
@@ -0,0 +1,18 @@
+package coffeshop;
+
+public interface Person {
+    
+    public void setName(String name);
+    public void setEmail(String email);
+    public void setGender(String gender);
+    public void setMobile(String mobile);
+    public void setAddress(String address);
+    public void setPassword(String password);
+    public String getName();
+    public String getEmail();
+    public String getGender();
+    public String getMobile();
+    public String getAddress();
+    public String getPassword();
+    
+}
